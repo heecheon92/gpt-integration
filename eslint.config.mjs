ab9c53b95625979836e114f9473dc00a9cdf9788
Heecheon Park <heecheon92@gmail.com>
2025-02-08 20:01:43 +0900

    📦️ chore: let eslint to extend prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,jsx,tsx}"],
     languageOptions: {
